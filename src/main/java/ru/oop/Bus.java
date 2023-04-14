badcbadb5d9f49b76b39dcb9bb38d5f045c2f628
Valera-2002 <bagman7787@gmail.com>
2023-04-14 23:43:01 +0500

    solve

---
+++ src/main/java/ru/oop/Bus.java
@@ -0,0 +1,21 @@
+package ru.oop;
+
+public class Bus implements Transport{
+    public Bus(String number, Person person) {
+    }
+
+    @Override
+    public Position getPosition() {
+        return null;
+    }
+
+    @Override
+    public void setPosition(Position position) {
+
+    }
+
+    @Override
+    public void go(Position destination) {
+
+    }
+}
---
+++ src/main/java/ru/oop/Car.java
@@ -0,0 +1,21 @@
+package ru.oop;
+
+public class Car implements Transport {
+    public Car(Person person) {
+    }
+
+    @Override
+    public Position getPosition() {
+        return null;
+    }
+
+    @Override
+    public void setPosition(Position position) {
+
+    }
+
+    @Override
+    public void go(Position destination) {
+
+    }
+}
